581815ae2633b16a5f44eab2f33b907f9fb57a84
Bruno Fernandes <bruno.m.c.fer@gmail.com>
2023-11-20 14:07:21 +0000

    the coach is rude

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,10 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @answer = params[:question].end_with?('?') ? 'Silly question, get dressed and go to work' : "I don't care, get dressed and go to work!" # rubocop:disable Layout/LineLength
+    @answer = 'Great!' if params[:question] == "I'm going to work"
+  end
+end
